7bf61269977dd17fa04d739150883f0c5b81d067
Manicca <mazdor14@gmail.com>
2015-04-26 21:07:25 +0400

    Worker form

--- Interiora/InterioraClient/EditPicture.Designer.cs
+++ Interiora/InterioraClient/EditPicture.Designer.cs
@@ -32,16 +32,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 145);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 84);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(428, 265);
+            this.pictureBox1.Size = new System.Drawing.Size(428, 326);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -63,42 +67,87 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Location = new System.Drawing.Point(23, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(102, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Ввод размерности";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 89);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(200, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Отметьте границы окон, дверей и т д ";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(252, 46);
+            this.button1.Location = new System.Drawing.Point(457, 367);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 43);
+            this.button1.Size = new System.Drawing.Size(131, 43);
             this.button1.TabIndex = 5;
             this.button1.Text = "Далее";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Edit
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отметить границы дверей";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 37);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Отметить границы помещения";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(446, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 37);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Отметить границы окна";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(446, 61);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 37);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Добавить дверь";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(446, 190);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 37);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Добавить окно";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // EditPicture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 424);
+            this.ClientSize = new System.Drawing.Size(593, 424);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "Edit";
+            this.Name = "EditPicture";
             this.Text = "Edit";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Edit_FormClosed);
             this.Load += new System.EventHandler(this.Edit_Load);
@@ -114,7 +163,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
