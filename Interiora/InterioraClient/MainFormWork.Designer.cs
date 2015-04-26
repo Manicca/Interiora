bb13a8ef5c1528381948bad0741d03ccedb69200
AlisVero <soxat73rus@gmail.com>
2015-04-26 21:08:15 +0300

    Add image to picturebox:)

--- Interiora/InterioraClient/MainFormWork.Designer.cs
+++ Interiora/InterioraClient/MainFormWork.Designer.cs
@@ -28,68 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(266, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Загрузить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(266, 94);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Выбрать из библиотеки";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(9, 25);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(246, 299);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(266, 150);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(126, 50);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "ОК";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // Main
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 341);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Main";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+               this.button1 = new System.Windows.Forms.Button();
+               this.button2 = new System.Windows.Forms.Button();
+               this.pictureBox1 = new System.Windows.Forms.PictureBox();
+               this.button3 = new System.Windows.Forms.Button();
+               this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+               ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // button1
+               // 
+               this.button1.Location = new System.Drawing.Point(550, 12);
+               this.button1.Name = "button1";
+               this.button1.Size = new System.Drawing.Size(129, 46);
+               this.button1.TabIndex = 0;
+               this.button1.Text = "Загрузить";
+               this.button1.UseVisualStyleBackColor = true;
+               this.button1.Click += new System.EventHandler(this.button1_Click);
+               // 
+               // button2
+               // 
+               this.button2.Location = new System.Drawing.Point(550, 64);
+               this.button2.Name = "button2";
+               this.button2.Size = new System.Drawing.Size(129, 50);
+               this.button2.TabIndex = 1;
+               this.button2.Text = "Выбрать из библиотеки";
+               this.button2.UseVisualStyleBackColor = true;
+               // 
+               // pictureBox1
+               // 
+               this.pictureBox1.Image = global::InterioraClient.Properties.Resources._2dkRaUJ9ArI;
+               this.pictureBox1.InitialImage = null;
+               this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+               this.pictureBox1.Name = "pictureBox1";
+               this.pictureBox1.Size = new System.Drawing.Size(532, 330);
+               this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+               this.pictureBox1.TabIndex = 2;
+               this.pictureBox1.TabStop = false;
+               // 
+               // button3
+               // 
+               this.button3.Location = new System.Drawing.Point(550, 120);
+               this.button3.Name = "button3";
+               this.button3.Size = new System.Drawing.Size(129, 50);
+               this.button3.TabIndex = 3;
+               this.button3.Text = "ОК";
+               this.button3.UseVisualStyleBackColor = true;
+               this.button3.Click += new System.EventHandler(this.button3_Click);
+               // 
+               // openFileDialog1
+               // 
+               this.openFileDialog1.FileName = "openFileDialog1";
+               // 
+               // MainFormWork
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(691, 354);
+               this.Controls.Add(this.button3);
+               this.Controls.Add(this.pictureBox1);
+               this.Controls.Add(this.button2);
+               this.Controls.Add(this.button1);
+               this.Name = "MainFormWork";
+               this.Text = "Form1";
+               this.Load += new System.EventHandler(this.MainFormWork_Load);
+               ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+               this.ResumeLayout(false);
 
         }
 
--- Interiora/InterioraClient/MainFormWork.cs
+++ Interiora/InterioraClient/MainFormWork.cs
@@ -10,23 +10,30 @@ using System.Windows.Forms;
 
 namespace InterioraClient
 {
-    public partial class MainFormWork : Form
-    {
-        public MainFormWork()
-        {
-            InitializeComponent();
-        }
+     public partial class MainFormWork : Form
+     {
+          public MainFormWork()
+          {
+               InitializeComponent();
+          }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-        }
+          private void button1_Click(object sender, EventArgs e)
+          {
+               openFileDialog1.ShowDialog();
+               Bitmap targetImage = new Bitmap(openFileDialog1.FileName);
+               pictureBox1.Image = targetImage;
+          }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            var form = new InterioraClient.EditPicture();
-            form.Show();
-            this.Hide();
-        }
-    }
+          private void button3_Click(object sender, EventArgs e)
+          {
+               var form = new InterioraClient.EditPicture();
+               form.Show();
+               this.Hide();
+          }
+
+          private void MainFormWork_Load(object sender, EventArgs e)
+          {
+
+          }
+     }
 }
