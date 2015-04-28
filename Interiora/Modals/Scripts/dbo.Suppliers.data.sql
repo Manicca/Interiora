526384f70b90380aa3307eb914ad804836c2fee5
Manicca <mazdor14@gmail.com>
2015-04-28 17:02:59 +0400

    Заполнены БД

--- Interiora/Modals/Scripts/dbo.Suppliers.data.sql
+++ Interiora/Modals/Scripts/dbo.Suppliers.data.sql
@@ -1,3 +1,12 @@
 SET IDENTITY_INSERT [dbo].[Suppliers] ON
-INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (1, N'ФЕдя ', N'ровра', N'аваов', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (1, N'РосАкс', N'rosaks@mail.ru', N'Иванов Петр Ильич', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (2, N'ИКЕЯ', N'ikea@mail.com', N'Дмитриев Констанстин Николаевич', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (3, N'МебельСити', N'mebely@mail.ru', N'Кравченко Ольга Вячеславовна', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (4, N'СтройГрад', N'strgrad@mail.ru', N'Кириленко Евгений Борисович', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (5, N'Сарай', N'sarai@mail.ru', N'Захарцев Альберт Эдуардович', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (6, N'Ашан', N'ashan@mail.ru', N'Лебедев Валерий Владимирович', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (7, N'ДеревяшКа', N'derevSh@mail.ru', N'Носырев Матвей Игоревич', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (8, N'Винтики и Шпунтики', N'vinshpun@mail.ru', N'Кожухова Амалия Арсеньевна', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (9, N'Человек Паук', N'man@mail.ru', N'Буйнов Кирилл Михайлович', NULL)
+INSERT INTO [dbo].[Suppliers] ([SupplierId], [Name], [Mail], [DG], [Supplier_SupplierId]) VALUES (10, N'World Wide Web', N'wowiwe@mail.ru', NULL, NULL)
 SET IDENTITY_INSERT [dbo].[Suppliers] OFF
