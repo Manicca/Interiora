ef9893b13151876e155ce44382be64954ae20132
AlisVero <soxat73rus@gmail.com>
2015-04-28 11:22:51 +0300

    несколько не оч красявошных изменений
    Signed-off-by: AlisVero <soxat73rus@gmail.com>

--- Interiora/InterioraClient/WorkForm.Designer.cs
+++ Interiora/InterioraClient/WorkForm.Designer.cs
@@ -44,7 +44,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(464, 21);
+            this.label1.Location = new System.Drawing.Point(596, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 0;
@@ -52,9 +52,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 18);
+            this.button1.Location = new System.Drawing.Point(8, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 51);
+            this.button1.Size = new System.Drawing.Size(106, 57);
             this.button1.TabIndex = 1;
             this.button1.Text = "Авторасстановка";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,18 +62,18 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(307, 21);
+            this.button2.Location = new System.Drawing.Point(289, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 51);
+            this.button2.Size = new System.Drawing.Size(106, 57);
             this.button2.TabIndex = 2;
             this.button2.Text = "Ручной режим";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(120, 15);
+            this.label2.Location = new System.Drawing.Point(120, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 54);
+            this.label2.Size = new System.Drawing.Size(100, 57);
             this.label2.TabIndex = 3;
             this.label2.Text = "Введите необходимое количество рабочих мест";
             // 
@@ -89,7 +89,7 @@
             "Хаб",
             "Коммутатор",
             "Протянуть витую пару"});
-            this.listBox1.Location = new System.Drawing.Point(438, 49);
+            this.listBox1.Location = new System.Drawing.Point(554, 51);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(118, 303);
             this.listBox1.TabIndex = 4;
@@ -99,22 +99,23 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 34);
+            this.textBox1.Location = new System.Drawing.Point(226, 27);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
             this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 94);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(401, 312);
+            this.pictureBox1.Size = new System.Drawing.Size(536, 312);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(438, 358);
+            this.button3.Location = new System.Drawing.Point(566, 360);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(106, 51);
             this.button3.TabIndex = 7;
@@ -126,7 +127,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 427);
+            this.ClientSize = new System.Drawing.Size(684, 423);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
@@ -136,7 +137,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "WorkForm";
-            this.Text = "Work";
+            this.Text = "Рабочее пространство";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WorkForm_FormClosing_1);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
